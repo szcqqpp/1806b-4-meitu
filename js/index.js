b9334bea82e4954ac13019ec3afe1994c7f51395
szcqqpp <48516732+szcqqpp@users.noreply.github.com>
2019-04-04 16:07:21 +0800

    项目调整

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 $(function(){
+	$("#ment").load("indexcon.html");
 	$(".but").click(function(){
-		$("#ment").load("contact.html");
+		var num = $(this).index()
+		if (num == 0) {
+			$("#ment").load("indexcon.html");
+		}
+		if (num == 5) {
+			$("#ment").load("contact.html");
+		};
 	})
 })
