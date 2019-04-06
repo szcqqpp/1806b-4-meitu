03a916c5b7c5cd4e70e5a6615f900a4587b1cecc
szcqqpp <48516732+szcqqpp@users.noreply.github.com>
2019-04-06 16:15:06 +0800

    数据库接口

---
+++ php/news.php
@@ -0,0 +1,22 @@
+<?php 
+include_once "conc.php";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	$page = $_GET["page"];
+}
+$pageSize = 4;
+$querySty = "SELECT * FROM news order by id limit ".($page-1)*$pageSize.",$pageSize";
+$stet = mysqli_query($con,$querySty);
+if (!$stet) {
+	die("数据库查询失败".mysqli_error($con));
+}
+$data = array();
+while ( $row = mysqli_fetch_assoc($stet)) {
+	$data[] = $row;
+}
+$jsonStr = json_encode(array(
+	"resultCode"=>200,
+	"message"=>"成功",
+	"data"=>$data
+	),JSON_UNESCAPED_UNICODE);
+echo $jsonStr;
+mysqli_close($con);
