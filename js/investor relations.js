6bbc884c21903c7654cca92ce7bc3daad6f8ff9d
S-Queen <1399018770@qq.com>
2019-04-07 19:20:43 +0800

    create new page

--- js/investor relations.js
+++ js/investor relations.js
@@ -2,4 +2,15 @@ $(function () {
     $(".nav li").click(function () {
         $(this).addClass("active").siblings().removeClass("active");
     });
+    $(".box1 .class").click(function () {
+        if ($(".box1 .class ul").css("display") == "none") {
+            $(".box1 .class ul").css("display", "block");
+        } else {
+            $(".box1 .class ul").css("display", "none");
+        }
+    });
+    $.get("investor relations.php", function (data) {
+        var datalist = JSON.parse(data);
+        console.log(datalist);
+    });
 });
